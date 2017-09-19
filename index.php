35253aa48eb428c997ea250b137d5986dcd584d7
skazkiful <30957783+skazkiful@users.noreply.github.com>
2017-09-19 11:38:12 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Пустая страница</title>
+        <meta charset="utf-8" />
+        <link href="/style.css" rel="stylesheet">
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+    </head>
+    <body>
+ 
+    </body>
+    <script src="http://yastatic.net/jquery/2.1.4/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</html>
