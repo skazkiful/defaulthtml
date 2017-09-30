0653200ee5185041beceb8f12367df5c541d7a47
skazkiful <30957783+skazkiful@users.noreply.github.com>
2017-09-30 19:08:44 +0300

    Update index.php

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
         <meta charset="utf-8" />
         <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
         <link href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css" rel="stylesheet">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     </head>
     <body>
  
