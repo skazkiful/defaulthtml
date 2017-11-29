cf22cbc483d3e8b9672c6fd7e71d962a559fd3c6
skazkiful <30957783+skazkiful@users.noreply.github.com>
2017-11-29 16:26:11 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <!doctype html>
 <html>
     <head>
-        <title>Пустая страница</title>
+        <title>Title</title>
         <meta charset="utf-8" />
         <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
         <link href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css" rel="stylesheet">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
     </head>
     <body>
  
