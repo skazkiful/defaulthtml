684f0f575bb6c27f5c53a33e03ca0c3d629c6de9
skazkiful <30957783+skazkiful@users.noreply.github.com>
2017-12-05 14:50:29 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,22 @@
 <!doctype html>
 <html>
-    <head>
-        <title>Title</title>
-        <meta charset="utf-8" />
-        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css" rel="stylesheet">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-    </head>
-    <body>
- 
-    </body>
-    <script src="http://yastatic.net/jquery/2.1.4/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <script>
-        $('a[href^="#"]').click(function(){
-            var target = $(this).attr('href');
-            $('html, body').animate({scrollTop: $(target).offset().top}, 800);
-            return false;
-        });    
-    </script>
+	<head>
+		<title>Каталог</title>
+		<meta charset="utf-8" />
+		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+		<link href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css" rel="stylesheet">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+	</head>
+	<body>
+		
+	</body>
+	<script src="http://yastatic.net/jquery/2.1.4/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<script>
+		$('a[href^="#"]').click(function(){
+			var target = $(this).attr('href');
+			$('html, body').animate({scrollTop: $(target).offset().top}, 800);
+			return false;
+		});    
+	</script>
 </html>
